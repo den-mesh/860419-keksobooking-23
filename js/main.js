2b0879c8c94aa361a3a1d67a1f43b3e61f80c4a9
den-mesh <dmesh81@gmail.com>
2021-05-30 13:05:16 +0400

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandomNumber(min, max) {
   if (max <= min) {
     return 'Значение max не может быть меньше, чем значение min, или равное ему';
-  } else if (min >= 0 && max >= 0) {
+  } else if (min >= 0 && max > 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются, источник developer.mozilla.org
   }
 }
@@ -10,7 +10,7 @@ function getRandomNumber(min, max) {
 function getRandomFractionalNumber(min, max, num) {
   if (max <= min) {
     return 'Значение max не может быть меньше, чем значение min, или равное ему';
-  } else if (min >= 0 && max >= 0) {
+  } else if (min >= 0 && max > 0) {
     const randomNumber = Math.random() * (max - min + 1) + min;
     return Number(randomNumber.toFixed(num));
   }
