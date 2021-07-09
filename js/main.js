0c27149a50bcf46d2b03acd8d558d12e5aa3d89b
den-mesh <dmesh81@gmail.com>
2021-07-09 21:04:24 +0400

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {createOffer} from './data.js';
+import './popup';
 
 const SIMILAR_OFFER_COUNT = 10;
 const similarOffers = new Array(SIMILAR_OFFER_COUNT).fill(null).map(() => createOffer());
